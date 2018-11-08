f8f39d57662736a5a0623183231ef7f2debc5254
tgadf <tgadfort@gmail.com>
2018-11-08 11:08:53 -0600

    Cool new network code

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setup(
     'Topic :: Software Development :: Libraries :: Python Modules',
     'Topic :: Utilities'
   ],
-  install_requires=['utils==0.0.1', 'geocluster==0.0.1', 'networkX', 'haversine', 'pandas', 'numpy'],
+  install_requires=['utils==0.0.1', 'geocluster==0.0.1', 'networkx>=2.2', 'haversine>=1.0.1', 'pandas>=0.23.4', 'numpy>=0.15.4'],
   dependency_links=['git+ssh://git@github.com/tgadf/utils.git#egg=utils-0.0.1', 'git+ssh://git@github.com/tgadf/geocluster.git#egg=geocluster-0.0.1']
 )
  
